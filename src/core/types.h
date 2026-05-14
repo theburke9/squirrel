d876c6ebeb90438d10f7fade63c1764a4cc43598
Jayson M <jayson@reeflect.fr>
2026-05-14 10:21:07 +0200

    feat(core): add internal types

---
+++ src/core/types.h
@@ -0,0 +1,41 @@
+#ifndef SQUIRREL_INTERNAL_TYPES_H
+#define SQUIRREL_INTERNAL_TYPES_H
+
+#include <assert.h>
+
+#include <stdint.h>
+#include <stddef.h>
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef float f32;
+typedef double f64;
+
+typedef size_t usize;
+typedef ptrdiff_t isize;
+
+static_assert(sizeof(i8) == 1, "i8 must be 1 byte");
+static_assert(sizeof(i16) == 2, "i16 must be 2 bytes");
+static_assert(sizeof(i32) == 4, "i32 must be 4 bytes");
+static_assert(sizeof(i64) == 8, "i64 must be 8 bytes");
+
+static_assert(sizeof(u8) == 1, "u8 must be 1 byte");
+static_assert(sizeof(u16) == 2, "u16 must be 2 bytes");
+static_assert(sizeof(u32) == 4, "u32 must be 4 bytes");
+static_assert(sizeof(u64) == 8, "u64 must be 8 bytes");
+
+static_assert(sizeof(f32) == 4, "f32 must be 4 bytes");
+static_assert(sizeof(f64) == 8, "f64 must be 8 bytes");
+
+static_assert(sizeof(usize) == sizeof(void*), "usize must match pointer size");
+static_assert(sizeof(isize) == sizeof(void*), "isize must match pointer size");
+
+#endif // SQUIRREL_INTERNAL_TYPES_H
