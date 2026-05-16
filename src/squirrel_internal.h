57559a476fc2ecef8fe000e4b52c4d846720ea5c
Jayson M <jayson@reeflect.fr>
2026-05-16 22:02:57 +0200

    refactor(core): rename core/tensor.h and core/types.h into core/tensor_internal.h and core/types_internal

--- src/squirrel_internal.h
+++ src/squirrel_internal.h
@@ -3,8 +3,8 @@
 
 #include "squirrel.h"
 #include "core/mem.h"
-#include "core/types.h"
+#include "core/types_internal.h"
 #include "core/storage.h"
-#include "core/tensor.h"
+#include "core/tensor_internal.h"
 
 #endif // SQUIRREL_INTERNAL_H
